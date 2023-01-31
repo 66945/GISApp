60ca010dc49e6a2a24a62df278ce287f99d0c3bb
66945 <dtmcgra@gmail.com>
2023-01-31 09:35:06 -0700

    Initial Commit

---
+++ GISApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GISApp
+//
+//  Created by McGrath, Daniel - Student on 1/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
